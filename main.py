a9acd224b89191d0b8bb09c4056028a926a69765
AndreyPernikov <Andrey.pernikov@mail.ru>
2025-11-11 18:10:13 +0300

    query-params (ч2)

--- main.py
+++ main.py
@@ -59,3 +59,25 @@ async def read_item_id(item_id: str, q: str | None = None):
     if q:
         return {"item_id": item_id, "q": q}
     return {"item_id": item_id}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: int, q: str | None = None, short: bool = False):
+    item = {"user_id": user_id, "item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
+
+
+@app.get("/i/items/{item_id}")
+async def read_item_needy(item_id: int, needy: str):
+    item = ({"item_id": item_id, "needy": needy})
+    return item
+
+
+@app.get("/it/items/{item_id}")
+async def read_item_it(item_id: str, needy: str | None = None, skip: int = 0, limit: int | None = None):
+    item = ({"item_id": item_id, "needy": needy, "skip": skip, "limit": limit})
+    return item
